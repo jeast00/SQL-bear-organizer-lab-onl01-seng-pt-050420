35f4c6c556adb6892aab7dec0bd1873757a0f344
Jonathan East <jonathan_east@ymail.com>
2020-06-26 17:38:26 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+-- Bears need a name, age, gender, color, temperament and alive
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "male", "chocolate", "calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "male", "chocolate", "calm", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 2, "male", "chocolate", "calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 2, "male", "chocolate", "calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 2, "male", "chocolate", "calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 2, "male", "chocolate", "calm", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 2, "male", "chocolate", "calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, NULL, "male", "brown", "aggressive", 0);
